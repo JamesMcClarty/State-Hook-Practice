ed8d2461c663cbf0cb1e39ef73ebbd2ead7379e6
James McC <mariobud@gmail.com>
2020-03-25 15:20:28 -0500

    "Adding Dispatched Filters"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 import uuid from 'uuid/v4';
 const initialTodos = [
   {
@@ -23,6 +23,7 @@ const App = () => {
   //States
   const [todos, setTodos] = useState(initialTodos);
   const [task, setTask] = useState('');
+  const [filter, dispatchFilter] = useReducer(filterReducer,'ALL');
 
   //Labels the todo as complete from the id
   const handleChangeCheckbox = id => {
@@ -38,6 +39,10 @@ const App = () => {
     );
   };
 
+  //Todo Filter
+
+  const filteredTodos
+
   //Event method to handle input on every change
   const handleChangeInput = event => {
     setTask(event.target.value)
@@ -52,8 +57,50 @@ const App = () => {
     event.preventDefault();
   };
 
+  //Complex State Hook Functions
+
+  const handleShowAll = () => {
+    dispatchFilter({type: 'SHOW_ALL'})
+  };
+
+  const handleShowComplete = () =>{
+    dispatchFilter({type: 'SHOW_COMPLETE'})
+  };
+
+  const handleShowIncomplete = () =>{
+    dispatchFilter({type: 'SHOW_INCOMPLETE'})
+  }
+
+  //Fiter reducer
+
+  const filterReducer = (state, action) =>{
+    switch(action.type){
+      case 'SHOW_ALL':
+        return 'ALL';
+      case 'SHOW_COMPLETE':
+        return 'COMPLETE';
+      case 'SHOW_INCOMPLETE':
+        return 'INCOMPLETE';
+      default:
+        throw new Error();
+    }
+  };
+
   return(
   <div>
+
+    <div>
+      <button type="button" onClick={handleShowAll}>
+        Show All
+      </button>
+      <button type="button" onClick={handleShowComplete}>
+        Show Complete
+      </button>
+      <button type="button" onClick={handleShowIncomplete}>
+        Show Incomplete
+      </button>
+    </div>
+
     <ul>
       {todos.map(todo => (
         <li key={todo.id}>
